0159f8edd613ae8e553fd0c2239521b02ff364f3
honglixi <boy2314@foxmail.com>
2021-04-22 15:37:48 +0800

    添加后台讲师管理模块

---
+++ service/service_edu/src/main/java/cn/yzaaa/eduservice/EduApplication.java
@@ -0,0 +1,17 @@
+package cn.yzaaa.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Honglixi
+ * @create 2021-04-22-14:24
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"cn.yzaaa"})
+public class EduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class,args);
+    }
+}
---
+++ service/service_edu/src/main/java/cn/yzaaa/eduservice/config/EduConfig.java
@@ -0,0 +1,23 @@
+package cn.yzaaa.eduservice.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Honglixi
+ * @create 2021-04-22-14:27
+ */
+@Configuration
+@MapperScan("cn.yzaaa.eduservice.mapper")
+public class EduConfig {
+    /**
+     * 逻辑删除插件
+     */
+    @Bean
+    public ISqlInjector sqlInjector(){
+        return new LogicSqlInjector();
+    }
+}

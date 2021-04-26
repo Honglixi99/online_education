0b2a48b3f647258e5d75ef40504ebec5b99ceed9
honglixi <boy2314@foxmail.com>
2021-04-26 21:29:28 +0800

    添加对前端的连接

---
+++ service/service_edu/src/main/java/cn/yzaaa/eduservice/controller/EduLoginController.java
@@ -0,0 +1,23 @@
+package cn.yzaaa.eduservice.controller;
+
+import cn.yzaaa.commonutils.R;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Honglixi
+ * @create 2021-04-25-12:35
+ */
+@RestController
+@RequestMapping("/eduservice/user")
+@CrossOrigin //解决跨越问题
+public class EduLoginController {
+
+    @PostMapping("login")
+    public R login(){
+        return R.ok().data("token","admin");
+    }
+    @GetMapping("info")
+    public R info(){
+        return R.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
